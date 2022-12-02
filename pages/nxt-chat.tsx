922863d9dd2cb925e12ba1be1a5310a9cbf42483
unbanksytv <livethelife.tv@gmail.com>
2022-12-02 14:35:03 +0100

    first commit

---
+++ pages/nxt-chat.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Chat from './chat';
+
+const NextChat: NextPage = () => {
+    const { query }: any = useRouter();
+
+    return (
+        <div>
+            <main>
+                <Chat OPENAI_API_KEY={query?.OPENAI_API_KEY} OPEN_AI_ORG={query?.OPEN_AI_ORG} model={query?.engine} />
+            </main>
+        </div>
+    )
+}
+
+export default NextChat;
